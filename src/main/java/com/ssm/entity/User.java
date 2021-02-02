f6f27fcc522049ebd3c5079590477b2ad4c70a5a
liqiang <liqiang@yunxi.tv>
2021-02-02 16:53:01 +0800

    fix

---
+++ src/main/java/com/ssm/entity/User.java
@@ -0,0 +1,74 @@
+package com.ssm.entity;
+
+import com.alibaba.excel.metadata.BaseRowModel;
+
+import java.io.Serializable;
+
+/**
+ * Created by liqiang on 2020/02/02
+ * @author smfx1314
+ *
+ */
+public class User extends BaseRowModel implements Serializable {
+
+    private Integer id;
+
+    private String username;
+
+    private String sex;
+
+    private String city;
+
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", sex='" + sex + '\'' +
+                ", city='" + city + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/ssm/service/impl/UserServiceImpl.java
@@ -0,0 +1,74 @@
+package com.ssm.service.impl;
+
+import com.ssm.dao.UserMapper;
+import com.ssm.entity.User;
+import com.ssm.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Created by liaiqng on 2020/02/02
+ */
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 查询所有用户
+     * @return
+     */
+    @Override
+    public List<User> getAllUser() {
+        List<User> allUser = userMapper.getAllUser();
+        return allUser;
+    }
+
+    /**
+     * 校验用户名
+     * @param username
+     * @return
+     */
+    @Override
+    public User checkUserName(String username) {
+
+        return userMapper.checkUserName(username);
+    }
+
+    /**
+     * 保存用户
+     * @param user
+     * @return
+     */
+    @Override
+    public int saveUser(User user) {
+        int i = userMapper.saveUser(user);
+        return i;
+    }
+
+    /**
+     * 修改用户
+     */
+    @Override
+    public void updateUser(User user) {
+
+        userMapper.updateUser(user);
+    }
+
+    /**
+     * 根据id删除用户
+     * @param id
+     * @return
+     */
+    @Override
+    public int deleteUser(Integer id) {
+
+        return  userMapper.deleteUser(id);
+    }
+
+}
